66208db9411bc27baecdb4749e1c68fe440666ad
Nihil-01 <64413865+Nihil-01@users.noreply.github.com>
2020-08-08 11:26:33 +0200

    Fix Get Image

--- src/components/dettaglioAnnuncio.component.js
+++ src/components/dettaglioAnnuncio.component.js
@@ -69,7 +69,7 @@ export default class DettaglioAnnuncio extends Component {
         this.datiPrenotazione = this.props.location.state[1];
 
         // Carica le immagini dell'annuncio dentro listOfImages
-        const path = require.context('../../images', true)
+        const path = require.context('https://team-mars.s3.eu-west-3.amazonaws.com/images', true)
         for (let i = 0; i < 5; i++) {
             try {
                 this.listOfImages.push(path('./ID' + this.state.idAnnuncio + '/img' + i + '.png'))
